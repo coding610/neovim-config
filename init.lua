b2b4f55f10bb1f49c8fdc7198f2f596379d91706
Sixten <Siggbo@Buyns-MacBook-Pro.local>
2022-11-14 20:06:52 +0100

    keybind fix

--- init.lua
+++ init.lua
@@ -3,13 +3,13 @@ require('Standard.Settings')
 require('Standard.Plugins')
 require('Standard.Mappings')
 
--- Additionals
 require('Additionals.Term')
 require('Additionals.Tree')
 require('Additionals.Dashboard')
 require('Additionals.Telescope')
 require('Additionals.DevIcons')
 require('Additionals.Telescope.filetype')
+
 
 -- Deco
 require('Deco.Lualine')
--- lua/AutoComplete/cmp.lua
+++ lua/AutoComplete/cmp.lua
@@ -7,44 +7,24 @@ cmp.setup {
       luasnip.lsp_expand(args.body)
     end,
   },
+
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
-    },
-    ['<Tab-t-t-t-t-t-t-t-t-t-t>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    }
   }),
+
   sources = {
     { name = 'nvim_lsp', max_item_count=5 },
     { name = 'luasnip', max_item_count=5 },
-    { name = 'buffer' },
-    { name = 'cmdline' },
-    { name = 'path' },
+    { name = 'buffer', max_item_count=5 },
+    { name = 'cmdline', max_item_count=5 },
+    { name = 'path', max_item_count=5 },
   },
 
-
-  window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-  }
+--  window = {
+--      completion = cmp.config.window.bordered(),
+--      documentation = cmp.config.window.bordered(),
+--  }
 }
